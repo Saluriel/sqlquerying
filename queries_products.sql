286cef6a3c423dcd5a0bf4cbcf155d70e7bb7375
root <root@DESKTOP-RJ1OF28.localdomain>
2022-06-14 14:51:01 -0400

    first commit

---
+++ queries_products.sql
@@ -0,0 +1,16 @@
+-- Comments in SQL Start with dash-dash --
+
+--1. insert into products (name, price, can_be_returned) values ('chair', 44.00, 'false');
+--2. insert into products (name, price, can_be_returned) values ('stool', 25.99, 'true');
+--3. insert into products (name, price, can_be_returned) values ('table', 124.00, 'false');
+--4. select * from products;
+--5. select name from products;
+--6. select name, price from products;
+--7. insert into products (name, price, can_be_returned) values ('bed', 800.00, 'true');
+--8. select can_be_returned, name from products where can_be_returned = 'True';
+--9. select name, price from products where price < 44.00;
+--10. select name, price  from products where price between 22.50 and 99.99;
+--11. update products set price = price - 20;
+--12. delete from products where price < 25;
+--13. update products set price = price + 20;
+--14. update products set can_be_returned = 'True';
